7cb8f3877ebb8ab1d72c397406e0994c47896fd7
Bryant Menn <bryant.menn@gmail.com>
2015-12-02 03:16:37 -0500

    docker: Added more hadoop docker stuff. Almost done, needs automatic start in container of hadoop services.

---
+++ bin/get-lib.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -o docker/hadoop/consul-template_0.11.1_linux_amd64.zip https://releases.hashicorp.com/consul-template/0.11.1/consul-template_0.11.1_linux_amd64.zip
+curl -o docker/hadoop/hadoop-2.5.2.tar.gz http://mirrors.gigenet.com/apache/hadoop/common/hadoop-2.5.2/hadoop-2.5.2.tar.gz
